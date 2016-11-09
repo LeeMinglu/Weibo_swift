9e8cf31d55ce5fcef351cbc33c83d9c2dda627f1
LeeMinglu <402007353@qq.com>
2016-11-09 22:03:13 +0800

    重写cell的initWithStyle方法

--- Weibo_swift/Weibo_swift/Controller/ViewController.swift
+++ Weibo_swift/Weibo_swift/Controller/ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
     
    
     var tableView: UITableView!
     var screen: CGRect!
+    var weiboModel: [Weibo]!
+    let indentifier = "weibo"
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.screen = UIScreen.mainScreen().bounds
-        let tableViewFrame = screen.size
+       
         let tableview = UITableView.init(frame: screen)
         
         self.tableView = tableview
@@ -27,17 +29,40 @@ class ViewController: UIViewController {
         
         self.view.addSubview(self.tableView)
         
-        for weibo in WeiBoArray {
-            print("&^&&&&&&&&&&&&&" + weibo.name)
-        }
+        self.tableView.registerClass(UITableViewCell.in, forCellReuseIdentifier: indentifier)
+        
+        self.weiboModel = WeiBoArray
+//        
+//        for weibo in WeiBoArray {
+//            print("&^&&&&&&&&&&&&&" + weibo.name)
+//        }
        
     }
+    
+    // MARK: ----TABLEVIEW DELEGATE METHORD-----
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.weiboModel.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let weibo = self.weiboModel[indexPath.row]
+         print("&^&&&&&&&&&&&&&" + weibo.name)
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //懒加载模型数据
     lazy var WeiBoArray: [Weibo] = {
     
         let weibos: [Weibo]
--- Weibo_swift/Weibo_swift/View/LSWeiBoCell.swift
+++ Weibo_swift/Weibo_swift/View/LSWeiBoCell.swift
@@ -1,54 +1,69 @@
 //
-//  LSWeiBoCell.swift
+//  LSWeiboCell.swift
 //  Weibo_swift
 //
-//  Created by 李明禄 on 16/9/17.
+//  Created by 李明禄 on 16/11/8.
 //  Copyright © 2016年 SocererGroup. All rights reserved.
 //
 
 import UIKit
 
-class LSWeiBoCell: UITableViewCell {
+class LSWeiboCell: UITableViewCell {
+    // MARK: cell 子控件
+    let imageViewIcon: UIImageView!
+    let lbName: UILabel!
+    let lbText: UILabel!
+    let vipImageView: UIImageView!
+    let picImageView: UIImageView!
     
-    let icon: UIImage
-    let name: UILabel
-    let vip: UIImage
-    let weibo: Weibo
-  //  var _weiboFrame: LSWeiboFrame
- /*   var weiboFrame: LSWeiboFrame {
-        set(newValue){
-            self._weiboFrame = newValue
-            
-        }
-        
-        get {
-        
-            return self._weiboFrame
-        }
+  // MARK:  /*设置字体大小*/
+    let fontLbName = UIFont.systemFontOfSize(13)
+    let fontLbText = UIFont.systemFontOfSize(15)
     
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+       super.init(style: style, reuseIdentifier: reuseIdentifier)
+       if !self.isEqual(nil) {
+        //动态创建子控件
+        let iconImageView = UIImageView.init()
+        self.contentView.addSubview(iconImageView)
+        self.imageViewIcon = iconImageView
+        
+        let nameLabel = UILabel.init()
+        self.contentView.addSubview(nameLabel)
+        self.lbName = nameLabel
+        
+        let vipView = UIImageView.init()
+        self.contentView.addSubview(vipView)
+        vipView.image = UIImage.init(named: "vip")
+        self.vipImageView = vipView
+        
+        let textView = UILabel.init()
+        self.contentView.addSubview(textView)
+        //自动换行
+        textView.numberOfLines = 0
+        self.lbText = textView
+        
+        let picView = UIImageView.init()
+        self.contentView.addSubview(picView)
+        self.picImageView = picView
+    
+        }
     }
-*/
-    override func awakeFromNib()
-    {
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func awakeFromNib() {
         super.awakeFromNib()
-        self.backgroundColor = UIColor.redColor()
+        // Initialization code
     }
-    // Class 初始化
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?)
-    {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.backgroundColor = UIColor.blueColor()
-    }
-    
-    required init(coder aDecoder: NSCoder)
-    {
-        super.init(coder: aDecoder)!
-    
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
 }
